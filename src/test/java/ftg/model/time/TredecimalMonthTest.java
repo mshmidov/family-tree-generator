e966a51f5bc7340de93d255b71c27624fd0ef779
Max Shmidov <mshmidov@exadel.com>
2015-08-27 17:10:53 +0300

    Improved TredecimalMonthTest

--- src/test/java/ftg/model/time/TredecimalMonthTest.java
+++ src/test/java/ftg/model/time/TredecimalMonthTest.java
@@ -1,5 +1,6 @@
 package ftg.model.time;
 
+import ftg.util.IntegerRange;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
@@ -11,6 +12,14 @@ import static org.junit.Assert.assertThat;
 @RunWith(Theories.class)
 public class TredecimalMonthTest {
 
+    @Theory
+    public void monthHaveDateRange(TredecimalMonth month) {
+
+        final IntegerRange days = month.getDays();
+        assertThat(days, is(equalTo(IntegerRange.inclusive(
+                month.ordinal() * TredecimalCalendar.DAYS_IN_MONTH + 1,
+                (month.ordinal() + 1) * TredecimalCalendar.DAYS_IN_MONTH))));
+    }
 
     @Theory
     public void previousMonthIsCircular(TredecimalMonth month) {
