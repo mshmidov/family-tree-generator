eb6456516491424ff5d5e5f16822670ebd905e57
Max Shmidov <mshmidov@exadel.com>
2015-09-07 16:49:41 +0300

    Added provider for Simulation to ApplicationModule

--- ftg-application/src/main/java/ftg/application/ApplicationModule.java
+++ ftg-application/src/main/java/ftg/application/ApplicationModule.java
@@ -1,11 +1,28 @@
 package ftg.application;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import ftg.application.bootstrap.ConfigurationLoader;
+import ftg.model.World;
+import ftg.model.time.TredecimalDate;
+import ftg.simulation.Simulation;
+import ftg.simulation.configuration.Country;
+
+import java.util.List;
 
 public class ApplicationModule extends AbstractModule {
 
     @Override
     protected void configure() {
 
+    }
+
+    @Provides
+    @Singleton
+    public Simulation getSimulation() {
+        final List<Country> countries = new ConfigurationLoader("./config/").loadConfiguration("simulation-config.yml");
+        final World world = new World(new TredecimalDate(0));
+        return new Simulation(countries, world);
     }
 }
--- ftg-application/src/main/java/ftg/application/gui/dashboard/DashboardPresenter.java
+++ ftg-application/src/main/java/ftg/application/gui/dashboard/DashboardPresenter.java
@@ -1,10 +1,15 @@
 package ftg.application.gui.dashboard;
 
+import com.google.inject.Inject;
+import ftg.simulation.Simulation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 
 public class DashboardPresenter {
+
+    @Inject
+    private Simulation simulation;
 
     @FXML
     private ComboBox<Integer> simulationDurationField;
